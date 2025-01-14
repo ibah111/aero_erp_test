9385a442b4ccecf31e59e23bf89d3a6e9ef71c97
ibah <starlord77@mail.ru>
2025-01-14 10:17:37 +0300

    feat(models): models folder

---
+++ src/Modules/Databases/Sqlite.database/models/User.ts
@@ -0,0 +1,33 @@
+import { InferAttributes, InferCreationAttributes } from 'sequelize';
+import {
+  AllowNull,
+  AutoIncrement,
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+
+@Table({ tableName: 'Users' })
+export class User extends Model<
+  InferAttributes<User>,
+  InferCreationAttributes<User>
+> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id: number;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name: string;
+
+  @AllowNull(false)
+  @Column(DataType.DATE)
+  updatedAt: Date;
+
+  @AllowNull(false)
+  @Column(DataType.DATE)
+  createdAt: Date;
+}
---
+++ src/Modules/Databases/Sqlite.database/models/index.ts
@@ -0,0 +1,4 @@
+import { ModelCtor } from 'sequelize-typescript';
+import { User } from './User';
+
+export const models: ModelCtor[] = [User];
