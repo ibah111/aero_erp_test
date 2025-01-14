757e133544f01b7bdc075d5231f1c7c094a4f688
ibah <starlord77@mail.ru>
2025-01-14 14:12:06 +0300

    feat: removed FileModule from appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import FilesModule from './Pages/Files/Files.module';
 import { ModuleOfModules } from './Modules/Modules.module';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    FilesModule,
     ModuleOfModules,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'secretKey',
