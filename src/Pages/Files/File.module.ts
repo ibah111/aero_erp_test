72a8ee7666706d005c8c49fe06fcdafe58894956
ibah <starlord77@mail.ru>
2025-01-14 18:04:37 +0300

    fix: is not a function fix, make correct import

--- src/Pages/Files/File.module.ts
+++ src/Pages/Files/File.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import FileController from './File.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { FileService } from './File.services';
+import { File } from 'src/Modules/Databases/Sqlite.database/models/File';
 
 @Module({
   imports: [SequelizeModule.forFeature([File], 'sqlite')],
