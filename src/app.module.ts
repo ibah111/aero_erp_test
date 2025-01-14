a13f1b7f9069d06e8dceb777af47feb21a0313a9
ibah <starlord77@mail.ru>
2025-01-14 16:40:29 +0300

    feat: adding pages module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ModuleOfModules } from './Modules/Modules.module';
 import { JwtModule } from '@nestjs/jwt';
+import PagesModule from './Pages/Pages.module';
 
 @Module({
   imports: [
+    PagesModule,
     ModuleOfModules,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'secretKey',
