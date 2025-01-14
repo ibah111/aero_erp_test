dd0eecdcb48607899b3690c49ae9db658a98b636
ibah <starlord77@mail.ru>
2025-01-14 16:33:26 +0300

    feat: add to common pool

--- src/Modules/Databases/Sqlite.database/models/index.ts
+++ src/Modules/Databases/Sqlite.database/models/index.ts
@@ -1,4 +1,6 @@
 import { ModelCtor } from 'sequelize-typescript';
-import { Users } from './User';
+import { User } from './User';
+// Я назвал так модель файла, чтобы не путать с классом File из библиотеки Buffer
+import { File as FileModel } from './File';
 
-export const models: ModelCtor[] = [Users];
+export const models: ModelCtor[] = [User, FileModel];
