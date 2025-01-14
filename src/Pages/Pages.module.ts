bd6b8adb3e5da0f558c9f5fa26f97d46b6342c41
ibah <starlord77@mail.ru>
2025-01-14 18:04:59 +0300

    feat: renamed

--- src/Pages/Pages.module.ts
+++ src/Pages/Pages.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import UsersModule from './Users/Users.module';
-import FilesModule from './Files/Files.module';
+import FilesModule from './Files/File.module';
 
 @Module({
   imports: [UsersModule, FilesModule],
