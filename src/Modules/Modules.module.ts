78a2f98ddf25a69ba5d1d4f79a5bfe10fe55db34
ibah <starlord77@mail.ru>
2025-01-15 15:58:17 +0300

    feat: remove jsw init from app to auth module

--- src/Modules/Modules.module.ts
+++ src/Modules/Modules.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './Databases/Databases.module';
+import AuthModule from './Auth/Auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
 })
 export class ModuleOfModules {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ModuleOfModules } from './Modules/Modules.module';
-import { JwtModule } from '@nestjs/jwt';
 import PagesModule from './Pages/Pages.module';
 
 @Module({
-  imports: [
-    PagesModule,
-    ModuleOfModules,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'secretKey',
-      signOptions: {
-        expiresIn: '10m',
-      },
-    }),
-  ],
+  imports: [PagesModule, ModuleOfModules],
 })
 export class AppModule {}
