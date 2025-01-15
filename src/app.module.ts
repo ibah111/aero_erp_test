74e96e2c23cc22711a0a809cbfb76af5cce5b3f6
ibah <starlord77@mail.ru>
2025-01-15 17:27:26 +0300

    feat: config module root is global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ModuleOfModules } from './Modules/Modules.module';
 import PagesModule from './Pages/Pages.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PagesModule, ModuleOfModules],
+  imports: [
+    PagesModule,
+    ModuleOfModules,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
