eae8d630d36cde9757976fea9791eec0351bdcee
ibah <starlord77@mail.ru>
2025-01-15 17:40:24 +0300

    feat: to lower case

--- src/Modules/Databases/Sqlite.database/seeds/001-user-seed.ts
+++ src/Modules/Databases/Sqlite.database/seeds/001-user-seed.ts
@@ -2,7 +2,7 @@ import { QueryInterface } from 'sequelize';
 import { MigrationFn } from 'umzug';
 const default_pull_of_users = [
   {
-    login: 'Admin',
+    login: 'admin',
     password: 'password',
     createdAt: new Date(),
     updatedAt: new Date(),
