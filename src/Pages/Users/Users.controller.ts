2b6c4f24f03bcaf4b210359a35ac7b617af74b02
ibah <109962513+ibah111@users.noreply.github.com>
2025-01-16 22:47:36 +0300

    feat: module init logs

--- src/Pages/Users/Users.controller.ts
+++ src/Pages/Users/Users.controller.ts
@@ -5,10 +5,20 @@ import UsersService from './Users.service';
 @ApiTags('Users')
 @Controller('Users')
 export default class UsersController implements OnModuleInit {
-  salt_value = process.env.SALT;
+  salt = process.env.SALT;
+  JWT_SECRET = process.env.JWT_SECRET;
+  REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY;
   constructor(private readonly service: UsersService) {}
   onModuleInit() {
-    console.log('salt'.yellow, this.salt_value);
+    console.log('salt'.yellow, '===', this.salt);
+    console.log(
+      'JWT_SECRET is'.yellow,
+      this.JWT_SECRET ? 'loaded'.green : 'empty'.red,
+    );
+    console.log(
+      'REFRESH_SECRET_KEY is'.yellow,
+      this.REFRESH_SECRET_KEY ? 'loaded'.green : 'empty'.red,
+    );
   }
 
   @Get('getUsers')
