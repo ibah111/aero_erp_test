bc1c219f8b1a32e722c802b5ff759ebb7ec26aef
ibah <109962513+ibah111@users.noreply.github.com>
2025-01-19 20:08:55 +0300

    feat: remove tokens service

--- src/Modules/Token/Token.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import TokenService from './Token.service';
-
-@Module({
-  exports: [TokenService],
-  providers: [TokenService],
-})
-export default class TokenModule {}
